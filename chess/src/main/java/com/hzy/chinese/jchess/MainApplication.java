aa860bcaa6d571d067db7b9b69d1e3fded231120
zhanguoxiong@daoran.tv <>
2024-07-23 18:24:11 +0800

    chess

---
+++ chess/src/main/java/com/hzy/chinese/jchess/MainApplication.java
@@ -0,0 +1,23 @@
+package com.hzy.chinese.jchess;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.Utils;
+
+/**
+ * Created by HZY on 2018/3/6.
+ */
+
+public class MainApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Utils.init(this);
+        LogUtils.getConfig().setLogSwitch(BuildConfig.DEBUG)
+                .setLogHeadSwitch(false).setBorderSwitch(false);
+    }
+}
